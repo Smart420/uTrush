79f2441ebe771265c298d4f69a40cb4ac2c8130e
Nitipoom Yaiwichai <nitipoom.yawichai@gmail.com>
2017-08-01 15:37:03 +0700

    NewOnefinal

--- uTrush/index.android.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import uTrash from './src/smart.js';
- 
- 
- 
-AppRegistry.registerComponent('uTrush', () => uTrash);
